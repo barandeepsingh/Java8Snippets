18afc4331d84b5f0af16a59c4b2c2aa2c4d33eb7
Barandeep Singh <barandeepsingh@gmail.com>
2020-07-28 21:54:53 +0530

    loop performance demo

---
+++ src/main/java/com/baran/java8/samples/loops/LoopsDemo.java
@@ -0,0 +1,54 @@
+package com.baran.java8.samples.loops;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Slf4j
+public class LoopsDemo {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 1_000_000_000;
+
+        LocalDateTime startTime = LocalDateTime.now();
+        log.info("Started execution");
+        int sum = IntStream.rangeClosed(start, end).sum();
+        log.info("Int stream sum is {}", sum);
+        LocalDateTime endTime = LocalDateTime.now();
+        log.info("Completed execution in {} milliseconds ", startTime.until((endTime), ChronoUnit.MILLIS));
+        log.info("===================");
+
+        startTime = LocalDateTime.now();
+        log.info("Started execution");
+        sum = IntStream.rangeClosed(start, end).parallel().reduce(0, Integer::sum);
+        log.info("Parallel IntStream sum is {}", sum);
+        endTime = LocalDateTime.now();
+        log.info("Completed execution in {} milliseconds ", startTime.until((endTime), ChronoUnit.MILLIS));
+        log.info("===================");
+
+        startTime = LocalDateTime.now();
+        log.info("Started execution");
+        sum = Stream.iterate(0,n->n+1).limit(end).reduce(0,(a,b)->(a+b));
+        log.info("Stream iterate sum is {}", sum);
+        endTime = LocalDateTime.now();
+        log.info("Completed execution in {} milliseconds ", startTime.until((endTime), ChronoUnit.MILLIS));
+        log.info("===================");
+
+
+        startTime = LocalDateTime.now();
+        sum = 0;
+        log.info("Started execution");
+        for (int i = 0; i < end; i++) {
+            sum += i;
+        }
+        log.info("For loop sum is {}", sum);
+        endTime = LocalDateTime.now();
+        log.info("Completed execution in {} milliseconds ", startTime.until((endTime), ChronoUnit.MILLIS));
+        log.info("===================");
+
+
+    }
+}
